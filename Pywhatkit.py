925fcdcf8fc00dcada962b0344607b006eb1f718
Manasvi Pant <88909122+ManasviPant09@users.noreply.github.com>
2021-08-17 04:05:45 +0530

    Update Pywhatkit.py

--- Pywhatkit.py
+++ Pywhatkit.py
@@ -1,6 +1,6 @@
 
 import pywhatkit as p
-#p.sendwhatmsg(f"+919997390499","This is a message for you.",3,56)
-#p.playonyt("Steal Your Girl")
+#p.sendwhatmsg(f"+91{phone_number}","This is a message for you.",3,56)  #Change the country code as per your convinience. 
+#p.playonyt("Steal Your Girl")                                          #Open a You-Tube Video
 #p.search("Google")
 p.info("Java")
